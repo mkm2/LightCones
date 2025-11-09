b18e5a4b15f0b738934eaba5ac99809c478e6e00
Maximilian Müllenbach <Maximilian.Muellenbach@googlemail.com>
2025-11-09 18:09:46 +0100

    Neel states

--- src/basics.jl
+++ src/basics.jl
@@ -14,7 +14,7 @@ export up, down, rightx, leftx, neel_state, neel_x_state
 export chainJ, chainJ_pbc, correlator, single_spin_op
 export xxz, xxz_pbc, xyz, xyz_pbc, field_term, hamiltonian_from_positions, const_field, field_term_from_dist, sample_hs
 export nearest_neighbourJ, nearest_neighbourJ_pbc
-export random_state, random_product_state, random_bit_state, random_bitstring_state, random_basis_state
+export random_state, random_product_state, random_bit_state, random_bitstring_state, random_basis_state,
 export magnetisation, fidelity, measure_at_j, measure_all, sign_of_eigenstate, signs_of_eigenstate, otoc_by_eigenstate_measurement
 
 
@@ -166,12 +166,10 @@ function random_bitstring_state(N::Int, axis::String) #simply choose random basi
 end
 
 function neel_state(N::Int)
-    N%2 == 0 || throw("N has to be even to create a Néel state.")
     return kron((i%2 == 1 ? up : down for i in 1:N)...)
 end
 
 function neel_x_state(N::Int)
-    N%2 == 0 || throw("N has to be even to create a Néel state.")
     return kron((i%2 == 1 ? rightx : leftx for i in 1:N)...)
 end
 
